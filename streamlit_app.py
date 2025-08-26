b76614ee9865248784ac77199d490d8748d78866
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-08-26 10:30:53 -0700

    Update streamlit_app.py
    Entrada de texto

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Aplicación de Procesamiento de Archivos de Texto")
+st.write("Sube un archivo .txt para procesarlo y ver los resultados en un DataFrame.")
+
+uploaded_file = st.file_uploader("Elige un archivo .txt", type=["txt"])
+
+# Entrada con exito
+if uploaded_file is not None:
+        st.write("Archivo subido con éxito.")
